46ee579133f019e3bace3140e15ed5dc64ffc65f
huyencao <caohuyeenf1997@gmail.com>
2019-09-07 08:04:16 +0700

    .

--- resources/views/vendor/pagination/simple-default.blade.php
+++ resources/views/vendor/pagination/simple-default.blade.php
@@ -4,14 +4,12 @@
         @if ($paginator->onFirstPage())
             <a><i class="fa fa-backward"></i>@lang('pagination.previous')</a>
         @else
-            <a href="{{ $paginator->previousPageUrl() }}" rel="prev"><i
-                        class="fa fa-backward"></i>@lang('pagination.previous')</a>
+            <a href="{{ $paginator->previousPageUrl() }}" rel="prev"><i class="fa fa-backward"></i>@lang('pagination.previous')</a>
         @endif
 
         {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
-            <a href="{{ $paginator->nextPageUrl() }}" rel="next">@lang('pagination.next')<i
-                        class="fa fa-forward"></i></a>
+            <a href="{{ $paginator->nextPageUrl() }}" rel="next">@lang('pagination.next')<i class="fa fa-forward"></i></a>
         @else
             <a>@lang('pagination.next')<i class="fa fa-forward"></i></a>
         @endif
